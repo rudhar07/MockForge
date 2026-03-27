1620c35da95367e1aa2136c017cd5ed56a58d54e
Rudhar <rudharbajaj@gmail.com>
2026-03-28 02:52:39 +0530

    relative path for URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// This securely points all our API calls to our Express server
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
 });
 
 export default API;
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
